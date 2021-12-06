13afb083ac0f364137395166fb419617856fe345
tripleh1801 <hoanghhh.01257989756@gmail.com>
2021-12-06 23:19:13 +0700

    design UI column trello

--- src/App.js
+++ src/App.js
@@ -1,7 +1,97 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="trello-root">
+      <nav className="navbar app">app bar</nav>
+      <nav className="navbar board">board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://pbs.twimg.com/media/EvdRuL3UUAAE5se?format=jpg&name=4096x4096"
+                alt="alt-img"
+              />
+              new itemcard
+            </li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+          </ul>
+          <footer>another card</footer>
+        </div>
+
+        <div className="column">
+          <header>brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://pbs.twimg.com/media/EvdRuL3UUAAE5se?format=jpg&name=4096x4096"
+                alt="alt-img"
+              />
+              Title
+            </li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+          </ul>
+          <footer>another card</footer>
+        </div>
+
+        <div className="column">
+          <header>brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://pbs.twimg.com/media/EvdRuL3UUAAE5se?format=jpg&name=4096x4096"
+                alt="alt-img"
+              />
+              new itemcard
+            </li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+          </ul>
+          <footer>another card</footer>
+        </div>
+
+        <div className="column">
+          <header>brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://pbs.twimg.com/media/EvdRuL3UUAAE5se?format=jpg&name=4096x4096"
+                alt="alt-img"
+              />
+              Title
+            </li>
+            <li>new itemcard</li>
+            <li>new itemcard</li>
+          </ul>
+          <footer>another card</footer>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
