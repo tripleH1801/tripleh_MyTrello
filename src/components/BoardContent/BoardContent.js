9ede904104ebbc1c74e225fe6432ba052d6d0c38
tripleh1801 <hoanghhh.01257989756@gmail.com>
2021-12-07 12:53:20 +0700

    render by sample data, sort data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,15 +1,34 @@
-import TaskColumn from "components/TaskColumn/TaskColumn";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./BoardContent.scss";
+import CardColumn from "components/CardColumn/CardColumn";
+import { initialData } from "actions/initialData.js";
+import { isEmpty } from "lodash";
+import { mapOrder } from "utilities/sorts";
 
 function BoardContent() {
+  const [board, setBoard] = useState({});
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    const BoardFromDb = initialData.boards.find(
+      (board) => board.id === "board-1"
+    );
+    if (BoardFromDb) {
+      setBoard(BoardFromDb);
+      setColumns(mapOrder(BoardFromDb.columns, BoardFromDb.columnorder, "id"));
+      console.log(columns);
+    }
+
+    return () => {};
+  }, []);
+  if (isEmpty(board)) {
+    return <div className="not-found">Board not found</div>;
+  }
   return (
     <div className="board-content">
-      <TaskColumn />
-      <TaskColumn />
-      <TaskColumn />
-      <TaskColumn />
-      <TaskColumn />
+      {columns.map((column, index) => (
+        <CardColumn key={index} column={column} />
+      ))}
     </div>
   );
 }
